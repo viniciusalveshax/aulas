98c69831876b33726577ad9e0fbaf99150452bba
Vinícius Alves Hax <viniciushax@gmail.com>
2024-06-24 16:53:43 -0300

    Complementando o exemplo

--- banco-de-dados-2/aula3/exemplo_join.sql
+++ banco-de-dados-2/aula3/exemplo_join.sql
@@ -21,6 +21,7 @@ INSERT INTO teacher (id, name, side) VALUES (4, 'Qui-gon Jin', 'Light');
 INSERT INTO teacher (id, name, side) VALUES (5, 'Obi Wan', 'Light');
 INSERT INTO teacher (id, name, side) VALUES (6, 'Anakin', 'Light');
 INSERT INTO teacher (id, name, side) VALUES (7, 'Luke', 'Light');
+INSERT INTO teacher (id, name, side) VALUES (8, 'Mace Windu', 'Light');
 
 INSERT INTO student (name, teacher_id) VALUES ('Luke', 2);
 INSERT INTO student (name, teacher_id) VALUES ('Anakin', 5);
@@ -30,10 +31,5 @@ INSERT INTO student (name, teacher_id) VALUES ('Qui-gon Jin', 3);
 INSERT INTO student (name, teacher_id) VALUES ('Ahsoka', 6);
 INSERT INTO student (name, teacher_id) VALUES ('Rei', 7);
 INSERT INTO student (name, teacher_id) VALUES ('Kylo Ren', 7);
-
-
-
-
-
-
+INSERT INTO student (name) VALUES ('Palpatine');
 
